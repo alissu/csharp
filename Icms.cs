0283657d7c5eea1ddf90b78e0cf4a38839726714
alissu <49723560+alissu@users.noreply.github.com>
2020-11-23 08:55:32 -0300

    Update Icms.cs

--- Icms.cs
+++ Icms.cs
@@ -3,6 +3,18 @@ namespace CalculosImpostos
   //TODO: criar pasta e fazer devidas configurações
   public class Impostos
   {
-    public decimal Icms(decimal valorProduto, decimal aliquota) => valorProduto / (1 - aliquota);
+    public decimal Icms(decimal valorProduto, bool isPorDentro, decimal aliquota){
+      var icmsARecolher = 0M;
+      if(isPorDentro)
+      {
+        icmsARecolher = valorProduto / (1 - aliquota) - valorProduto;
+        return icmsARecolher;
+      }
+      else
+      {
+        icmsARecolher = valorProduto * aliquota - valorProduto;
+        return icmsARecolher;
+      }
+    }
   }
 }
